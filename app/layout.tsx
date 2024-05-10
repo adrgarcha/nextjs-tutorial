5bd06c2a9b62f0f57447ef374a86d4a2fa72cefb
Adrián García Chavero <adrgarcha@alum.us.es>
2024-05-10 21:33:29 +0200

    Solve fonts import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/app/ui/global.css";
-import { inter } from "./ui/font";
+import { inter } from "./ui/fonts";
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { lusitana } from './ui/font';
 import Image from 'next/image';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
